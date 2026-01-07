709c37d3ce3ab65093a786f42252370432a294b3
darlingson <dmakuwila@gmail.com>
2026-01-07 21:13:20 +0200

    added persistence for reports

---
+++ Api/Migrations/20260107171410_addedReportsEntity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedReportsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportEvidences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReportId = table.Column<int>(type: "integer", nullable: false),
+                    FilePath = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportEvidences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    Narrative = table.Column<string>(type: "text", nullable: false),
+                    Impact = table.Column<string>(type: "text", nullable: false),
+                    AssignedTo = table.Column<string>(type: "text", nullable: false),
+                    ResolvedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportEvidences");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
---
+++ Api/Models/Report.cs
@@ -0,0 +1,25 @@
+namespace Api.Models
+{ 
+    public class Report {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public String Type { get; set; } = String.Empty;
+        public string Status { get; set; } = String.Empty;
+        public string Location { get; set; } = String.Empty;
+        public string Narrative { get; set; } = String.Empty;
+        public string Impact { get; set; } = String.Empty;
+        public string AssignedTo { get; set; } = String.Empty;
+        public DateTime? ResolvedAt { get; set; }
+        public string Description { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int CreatedBy { get; set; }
+    }
+    public class ReportEvidence{
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public string FilePath { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
