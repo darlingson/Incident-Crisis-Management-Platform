2e4980f20ed3502348d9770e71e45dffb95c4f75
darlingson <dmakuwila@gmail.com>
2026-01-09 19:39:20 +0200

    removed merge confict

--- Api/Data/ApplicationDbContext.cs
+++ Api/Data/ApplicationDbContext.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -60,44 +59,4 @@ namespace Api.Data
             );
         }
     }
-=======
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Api.Models;
-
-namespace Api.Data
-{
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, 
-        IdentityUserClaim<string>, ApplicationUserRole, IdentityUserLogin<string>, 
-        IdentityRoleClaim<string>, IdentityUserToken<string>>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<Report> Reports => Set<Report>();
-        public DbSet<ReportEvidence> ReportEvidences => Set<ReportEvidence>();
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<ApplicationUserRole>(userRole =>
-            {
-                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
-
-                userRole.HasOne(ur => ur.User)
-                    .WithMany(u => u.UserRoles)
-                    .HasForeignKey(ur => ur.UserId)
-                    .IsRequired();
-
-                userRole.HasOne(ur => ur.Role)
-                    .WithMany(r => r.UserRoles)
-                    .HasForeignKey(ur => ur.RoleId)
-                    .IsRequired();
-            });
-        }
-    }
->>>>>>> 681d14d (missed files)
 }
